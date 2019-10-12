6ec23e764c3a767652f1a17bb152db023e2ce752
Craig Swanson <cswanson02@me.com>
2019-10-11 21:56:09 -0500

    Implemented numberOfItemsInSection and cellForItemAt.

--- PhotoCollection/PhotoCollection/View Controllers/PhotosCollectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/PhotosCollectionViewController.swift
@@ -19,10 +19,6 @@ class PhotosCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
@@ -39,23 +35,16 @@ class PhotosCollectionViewController: UICollectionViewController {
     */
 
     // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return photoController.photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? PhotosCollectionViewCell else { fatalError("The cell was not found") }
+        
+        let photo = photoController.photos[indexPath.item]
+        cell.photo = photo
         return cell
     }
 
